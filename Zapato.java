de8ebb2b5363a6eb90c3494bfc50c2eff0614221
Mikecszx <65678884+Mikecszx@users.noreply.github.com>
2020-11-12 11:24:20 -0600

    Add files via upload
    Una empresa quiere calcular el precio de sus zapatos dependiendo el modelo y talla usando una clase para definir  al zapato y el main en venta zapatos

---
+++ Zapato.java
@@ -0,0 +1,16 @@
+package Zapato;
+
+public class Zapato {
+        //Atributos
+        String modelo;
+        int talla;
+        double precio;
+        //Constructor
+        public Zapato(String modeloL, int tallaL, double precioL) {
+        modelo= modeloL;
+        talla= tallaL;
+        precio= precioL;
+
+
+        } // end constructor
+}   // end class zapato
